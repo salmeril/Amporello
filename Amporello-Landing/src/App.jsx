f0a1c506106d48c106bc3086e58f3762ee6a175c
salmeril <54689767+salmeril@users.noreply.github.com>
2024-03-09 22:15:54 -0300

    d

--- Amporello-Landing/src/App.jsx
+++ Amporello-Landing/src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header />
+    </div>
+    
+
+ 
   )
 }
---
+++ Amporello-Landing/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+    <header className="flex intems-center w-full">
+    <div>
+          <div className="bg-red-400">Logo</div>
+      </div>
+      
+        <nav className="bg-slate-500">
+            nav
+        </nav>
+          
+    </header>
+
+
+
+  )
+}
+
+export default Header
